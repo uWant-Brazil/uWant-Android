cfedcf06a08c6958d727049b0ed1d41339fa3b7f
Felipe Bonezi <felipebonezi@gmail.com>
2014-07-13 22:34:51 -0300

    Register @ Picture
    Rotina para tirar ou pegar foto da galeria efetuando os efeitos para
    exibição.

--- app/src/main/java/br/com/uwant/flow/RegisterActivity.java
+++ app/src/main/java/br/com/uwant/flow/RegisterActivity.java
@@ -5,8 +5,19 @@ import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
+import android.graphics.Matrix;
+import android.graphics.Paint;
+import android.graphics.PorterDuff;
+import android.graphics.PorterDuffXfermode;
+import android.graphics.Rect;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.MenuItem;
@@ -19,6 +30,8 @@ import android.widget.Toast;
 
 import org.apache.http.impl.cookie.DateUtils;
 
+import java.io.File;
+
 import br.com.uwant.R;
 import br.com.uwant.models.classes.Person;
 import br.com.uwant.models.classes.User;
@@ -30,8 +43,10 @@ import br.com.uwant.models.cloud.models.RegisterModel;
 public class RegisterActivity extends ActionBarActivity implements View.OnClickListener, RadioGroup.OnCheckedChangeListener, IRequest.OnRequestListener {
 
     private static final int PICTURE_REQUEST_CODE = 9898;
+    private static final int GALLERY_REQUEST_CODE = 9797;
 
     private byte[] mPictureBytes;
+    private File mPicturePath;
     private User.Gender mGender;
 
     private EditText mEditTextLogin;
@@ -40,6 +55,7 @@ public class RegisterActivity extends ActionBarActivity implements View.OnClickL
     private EditText mEditTextPassword;
     private EditText mEditTextBirthday;
     private ImageView mImageViewPicture;
+    private ImageView mImageViewPictureDetail;
     private RadioGroup mRadioGroupGender;
     private ProgressDialog mProgressDialog;
 
@@ -60,6 +76,7 @@ public class RegisterActivity extends ActionBarActivity implements View.OnClickL
         mEditTextPassword = (EditText) findViewById(R.id.register_editText_password);
         mEditTextBirthday = (EditText) findViewById(R.id.register_editText_birthday);
 
+        mImageViewPictureDetail = (ImageView) findViewById(R.id.register_imageView_pictureDetail);
         mImageViewPicture = (ImageView) findViewById(R.id.register_imageView_picture);
         mImageViewPicture.setOnClickListener(this);
 
@@ -89,7 +106,20 @@ public class RegisterActivity extends ActionBarActivity implements View.OnClickL
                 break;
 
             case R.id.register_imageView_picture:
-                takePicture();
+                AlertDialog.Builder builder = new AlertDialog.Builder(this);
+                builder.setItems(new String[] { "Tirar foto", "Buscar na galeria" }, new DialogInterface.OnClickListener() {
+
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        if (i == 0) {
+                            takePicture();
+                        } else {
+                            openGallery();
+                        }
+                    }
+
+                });
+                builder.create().show();
                 break;
 
             default:
@@ -112,10 +142,10 @@ public class RegisterActivity extends ActionBarActivity implements View.OnClickL
 
     private void executeRegister() {
         String login = mEditTextLogin.getText().toString();
-        String password = mEditTextLogin.getText().toString();
-        String name = mEditTextLogin.getText().toString();
-        String mail = mEditTextLogin.getText().toString();
-        String birthday = mEditTextLogin.getText().toString();
+        String password = mEditTextPassword.getText().toString();
+        String name = mEditTextName.getText().toString();
+        String mail = mEditTextMail.getText().toString();
+        String birthday = mEditTextBirthday.getText().toString();
 
         RegisterModel model = new RegisterModel();
         model.setLogin(login);
@@ -129,7 +159,12 @@ public class RegisterActivity extends ActionBarActivity implements View.OnClickL
     }
 
     private void takePicture() {
-        Intent intentPicture = new Intent();
+        mPicturePath = new File(Environment.getExternalStoragePublicDirectory(
+                        Environment.DIRECTORY_PICTURES),
+                "uwant_picture");
+
+        Intent intentPicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        intentPicture.putExtra(MediaStore.EXTRA_OUTPUT, Uri.fromFile(mPicturePath));
         startActivityForResult(intentPicture, PICTURE_REQUEST_CODE);
     }
 
@@ -138,9 +173,100 @@ public class RegisterActivity extends ActionBarActivity implements View.OnClickL
         super.onActivityResult(requestCode, resultCode, data);
         if (resultCode == RESULT_OK) {
             if (requestCode == PICTURE_REQUEST_CODE) {
-                // TODO Pegar a foto...
+                decodePicture();
+            } else if (requestCode == GALLERY_REQUEST_CODE) {
+                Uri selectedImage = data.getData();
+                String[] filePathColumn = {
+                        MediaStore.Images.Media.DATA };
+
+                Cursor cursor = getContentResolver().query(
+                        selectedImage, filePathColumn, null, null, null);
+                cursor.moveToFirst();
+
+                int columnIndex = cursor.getColumnIndex(
+                        filePathColumn[0]);
+                String filePath = cursor.getString(columnIndex);
+                cursor.close();
+
+                mPicturePath = new File(filePath);
+                decodePicture();
             }
         }
+    }
+
+    private void decodePicture() {
+        mImageViewPictureDetail.setVisibility(View.VISIBLE);
+
+        // Obtém o tamanho da ImageView
+        int targetW = mImageViewPicture.getWidth();
+        int targetH = mImageViewPicture.getHeight();
+
+        // Obtém a largura e altura da foto
+        BitmapFactory.Options bmOptions =
+                new BitmapFactory.Options();
+        bmOptions.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(mPicturePath.getAbsolutePath(), bmOptions);
+
+        int photoW = bmOptions.outWidth;
+        int photoH = bmOptions.outHeight;
+
+        // Determina o fator de redimensionamento
+        int scaleFactor = Math.min(
+                photoW/targetW, photoH/targetH);
+
+        // Decodifica o arquivo de imagem em
+        // um Bitmap que preencherá a ImageView
+        bmOptions.inJustDecodeBounds = false;
+        bmOptions.inSampleSize = scaleFactor;
+        bmOptions.inPurgeable = true;
+
+        Bitmap bitmap = BitmapFactory.decodeFile(mPicturePath.getAbsolutePath(), bmOptions);
+        bitmap = cropToFit(bitmap);
+        bitmap = scale(bitmap);
+        bitmap = circle(bitmap);
+
+        mImageViewPicture.setImageBitmap(bitmap);
+    }
+
+    private Bitmap scale(Bitmap bitmap) {
+        return Bitmap.createScaledBitmap(bitmap, mImageViewPicture.getWidth(), mImageViewPicture.getHeight(), false);
+    }
+
+    private Bitmap cropToFit(Bitmap srcBmp) {
+        Bitmap output;
+        if (srcBmp.getWidth() >= srcBmp.getHeight()){
+            output = Bitmap.createBitmap(srcBmp, srcBmp.getWidth()/2 - srcBmp.getHeight()/2, 0, srcBmp.getHeight(), srcBmp.getHeight());
+        } else {
+            output = Bitmap.createBitmap(srcBmp, 0, srcBmp.getHeight()/2 - srcBmp.getWidth()/2, srcBmp.getWidth(), srcBmp.getWidth());
+        }
+        return output;
+    }
+
+    public Bitmap circle(Bitmap bitmap) {
+        Bitmap output = Bitmap.createBitmap(bitmap.getWidth(),
+                bitmap.getHeight(), Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(output);
+
+        final int color = 0xff424242;
+        final Paint paint = new Paint();
+        final Rect rect = new Rect(0, 0, bitmap.getWidth(), bitmap.getHeight());
+
+        paint.setAntiAlias(true);
+        canvas.drawARGB(0, 0, 0, 0);
+        paint.setColor(color);
+        canvas.drawCircle(bitmap.getWidth() / 2F, bitmap.getHeight() / 2F,
+                bitmap.getWidth() / 2.2F, paint);
+        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.SRC_IN));
+        canvas.drawBitmap(bitmap, rect, rect, paint);
+
+        return output;
+    }
+
+    public void openGallery() {
+        Intent intent = new Intent(
+                Intent.ACTION_GET_CONTENT);
+        intent.setType("image/*");
+        startActivityForResult(intent, GALLERY_REQUEST_CODE);
     }
 
     @Override
