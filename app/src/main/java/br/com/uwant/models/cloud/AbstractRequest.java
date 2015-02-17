19121331929a0a461109e42e89b8abc8c61ea5fc
Cleibson Gomes da Silva <cleibson.gomes.silva@gmail.com>
2015-02-17 12:15:54 -0300

    Refactor

--- app/src/main/java/br/com/uwant/models/cloud/AbstractRequest.java
+++ app/src/main/java/br/com/uwant/models/cloud/AbstractRequest.java
@@ -48,7 +48,7 @@ public abstract class AbstractRequest<K> {
     /**
      * Padrão das URLs de requisição.
      */
-    public static final String URL_COMMON = "http://192.168.25.2:9000/v1";
+    public static final String URL_COMMON = "http://192.168.0.12:9000/v1";
 
     /**
      * Header responsável por conter o token de autenticação para requisições.
